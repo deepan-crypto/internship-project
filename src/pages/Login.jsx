19deb825f21a9b6176960d311a0e367e8434a4a5
deepan-crypto <deepang467@gmail.com>
2025-12-11 13:55:22 +0530

    login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./../Styles/login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <form className="loginForm">
+        <h2>Login Page</h2>
+        <label>Email</label>
+        <input type="email" placeholder="Email" required />
+        <label>Password</label>
+        <input type="password" placeholder="Password" required />
+        <button className="loginButton" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
